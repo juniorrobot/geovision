37a81f90d4f4ce19aff4d1ff3f6de722bcadb196
John Robinson <jr@juniorrobot.com>
2017-02-06 12:02:20 -0800

    add basic geojson exporting

--- geovision.py
+++ geovision.py
@@ -1,5 +1,6 @@
 import argparse
 import cv2
+import geojson
 import math
 import numpy as np
 import sys
@@ -7,6 +8,7 @@ import sys
 class Debugger:
     def __init__(self):
         self._show_images = False
+        self._show_log = False
 
     def __enter__(self):
         return self
@@ -25,6 +27,14 @@ class Debugger:
     def show_images(self, show):
         self._show_images = show
 
+    @property
+    def show_log(self):
+        return self._show_log
+
+    @show_log.setter
+    def show_log(self, show):
+        self._show_log = show
+
     def image(self, img, window_name="image"):
         if self._show_images:
             cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
@@ -35,15 +45,15 @@ class Debugger:
         if self._show_images:
             cv2.line(img, line[0], line[1], (0, 255, 0))
 
+    def log(self, message):
+        if self._show_log:
+            print(message)
+
 
 class Detector:
     def __init__(self, debugger=Debugger()):
         self._debug = debugger
-        self._lines = []
 
-    @property
-    def lines(self):
-        return self._lines
 
     def detect_roads(self, image_path):
         img = cv2.imread(image_path, cv2.IMREAD_COLOR)
@@ -51,11 +61,37 @@ class Detector:
         canny = cv2.Canny(img_gray, threshold1=200, threshold2=255)
         self._debug.image(canny)
         filtered = cv2.HoughLinesP(canny, rho=1, theta=math.pi/2, threshold=80)
+
+        lines = []
         for line in filtered[0]:
             line = ((line[0], line[1]), (line[2], line[3]))
-            self._lines.append(line)
+            lines.append(line)
             self._debug.line(img, line)
         self._debug.image(img)
+        return lines
+
+
+class GIS:
+    def __init__(self, debugger=Debugger()):
+        self._debug = debugger
+        self._features = []
+
+    def add_lines(self, lines):
+        line_strings = []
+        for line in lines:
+            self._debug.log(line)
+            line_string = geojson.LineString([
+                (float(line[0][0]), float(line[0][1])),
+                (float(line[1][0]), float(line[1][1])),
+                ])
+            line_strings.append(line_string)
+        col = geojson.GeometryCollection(line_strings)
+        self._features.append(col)
+
+    def to_json(self):
+        feature_col = geojson.FeatureCollection(self._features)
+        self._debug.log(feature_col)
+        return str(feature_col)
 
 
 def load_options(argv):
@@ -75,10 +111,18 @@ def load_options(argv):
 def main(argv):
     with Debugger() as debug:
         args = load_options(argv)
-        if args.verbosity and args.verbosity > 0:
-            debug.show_images = True
+        if args.verbosity:
+            if args.verbosity > 0:
+                debug.show_log = True
+            if args.verbosity > 1:
+                debug.show_images = True
+
         detect = Detector(debug)
-        detect.detect_roads(args.image)
+        lines = detect.detect_roads(args.image)
+
+        gis = GIS(debug)
+        gis.add_lines(lines)
+        gis.to_json()
 
 
 if __name__ == "__main__":
